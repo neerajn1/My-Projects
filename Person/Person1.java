e78d7d4d8280907a14321341cf377c357f38e487
neerajn1 <neerajn1@umbc.edu>
2025-11-29 22:09:23 -0500

    Add files via upload

---
+++ Person/Person1.java
@@ -0,0 +1,45 @@
+public class Person {
+    // Private parts that are retrieved with getters and setters
+    private String name;
+    private int age;
+    private String gender;
+
+    // Constructor ('this' refers to the object Person itself)
+    public Person() {
+        this.name = "John Smith";
+        this.age = 20;
+        this.gender = "Male";
+    }
+
+    // Parameters assigned to the object (Overloaded constructor)
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    // Getters and Setters for all the things that are private
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ Person/testPerson1.java
@@ -0,0 +1,16 @@
+public class testPerson {
+    public static void main(String[] args) {
+        //2 Person objects one set with default values in code while second is custom values as per parameter constructor
+        Person john = new Person();
+        Person josephine = new Person("Josephine Smith", 30, "Female");
+
+        // Printing information for each Table object by accessing the attributes with getters
+        System.out.println("Person 1: " + john.getName() + "           Age: " + john.getAge() + "    Gender: " + john.getGender());
+        System.out.println("Person 2: " + josephine.getName() + "      Age: " + josephine.getAge() + "    Gender: " + josephine.getGender());
+
+        //Setters to modify any of the attributes
+        josephine.setAge(25);
+        System.out.print("Note: ");
+        System.out.println("Josephine's age was updated to " + josephine.getAge() +  " after document verification");
+    }
+}
